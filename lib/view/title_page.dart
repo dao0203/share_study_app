c0df0e2b8ede782fe977cc73deb35e1228a77b0a
dao0203 <youzaizuoteng872@gmail.com>
2022-10-03 09:55:49 +0900

    ログイン画面とタイトル画面の追加と進捗状況

---
+++ lib/view/title_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class TitlePage extends StatelessWidget {
+  const TitlePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
