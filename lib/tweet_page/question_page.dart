1cdb4cdba133d0b4f9b42f400486f27cc06b2e46
dao0203 <youzaizuoteng872@gmail.com>
2022-10-25 16:01:25 +0900

    [update]調整

--- lib/tweet_page/question_page.dart
+++ lib/tweet_page/question_page.dart
@@ -12,63 +12,74 @@ class QuestionPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    String qusetion;
     return Scaffold(
       appBar: AppBar(
         title: const Text(
           ("投稿"),
         ),
       ),
-      body: Center(
+      body: SingleChildScrollView(
         child: Column(
           children: [
-            Container(
-              //投稿画面の中身
-              width: 307,
-              height: 272,
-              decoration: BoxDecoration(
-                color: Colors.grey[100],
-                borderRadius: BorderRadius.circular(20),
-              ),
+            SizedBox(
+              height: 400,
+              child: Card(
+                //投稿画面の中身
 
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  //項目の中身
-                  Text("科目"),
-                  /*科目選択ボタン*/
-                  DropdownButton(items: const [
-                    DropdownMenuItem(
-                      child: Text("科目を選択して下さい"),
-                      value: '科目を選択して下さい',
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    //項目の中身
+                    Text("科目"),
+                    SizedBox(height: 10),
+                    /*科目選択ボタン*/
+                    DropdownButton(items: const [
+                      DropdownMenuItem(
+                        child: Text("科目を選択して下さい"),
+                        value: '科目を選択して下さい',
+                      ),
+                      DropdownMenuItem(
+                        child: Text("科目1"),
+                        value: '科目1',
+                      ),
+                      DropdownMenuItem(
+                        child: Text("科目2"),
+                        value: '科目2',
+                      ),
+                    ], onChanged: (String? value) {}),
+                    Text("質問内容"),
+                    SizedBox(height: 10),
+                    /**質問内容を投稿するテキスト */
+                    TextField(
+                      keyboardType: TextInputType.multiline,
+                      maxLines: null,
+                      decoration: InputDecoration(
+                          enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10),
+                            borderSide: BorderSide(color: Colors.grey),
+                          ),
+                          hintText: '質問を入力（230文字以内）',
+                          icon: Icon(Icons.question_mark)),
                     ),
-                    DropdownMenuItem(
-                      child: Text("科目1"),
-                      value: '科目1',
-                    ),
-                    DropdownMenuItem(
-                      child: Text("科目2"),
-                      value: '科目2',
-                    ),
-                  ], onChanged: (String? value) {}),
-                  Text("質問内容"),
-                  /**質問内容を投稿するテキスト */
-                  TextField(
-                    keyboardType: TextInputType.multiline,
-                    maxLines: null,
-                  ),
-                  /**写真アップロードボタン */
-                  ElevatedButton(
-                    onPressed: () {},
-                    child: Text("＋写真をアップロード"),
-                    style: ElevatedButton.styleFrom(
-                      alignment: Alignment(
-                        0,
-                        0,
+                    /**写真アップロードボタン */
+                    SizedBox(height: 20), //間隔を開ける
+                    ElevatedButton(
+                      onPressed: () {},
+                      child: Text("＋写真をアップロード"),
+                      style: ElevatedButton.styleFrom(
+                        alignment: Alignment(
+                          0,
+                          0,
+                        ),
                       ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
+            ),
+            SizedBox(
+              height: 50,
             ),
             /**投稿するボタン */
             ElevatedButton(
