4980886f5ebbace0798a41a75ca1b2b1c085aec2
dao0203 <youzaizuoteng872@gmail.com>
2022-11-13 11:48:41 +0900

    [add]uploadするためのファイル追加

---
+++ lib/tweet_page/picture_upload.dart
@@ -0,0 +1,8 @@
+/**
+ * 必要なパッケージのインストールとして下記を実行
+ * flutter pub add firebase_core    URL:https://pub.dev/packages/firebase_core/install
+ * flutter pub add firebase_auth    URL:https://pub.dev/packages/firebase_auth/install
+ * flutter pub add firebase_storage URL:https://pub.dev/packages/firebase_storage/install
+ * flutter pub add path_provider    URL:https://pub.dev/packages/path_provider/install
+ * 
+ */
