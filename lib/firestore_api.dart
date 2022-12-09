c07c4104388399b0d04097bc9eb9b86b9f1cb419
dao0203 <youzaizuoteng872@gmail.com>
2022-12-09 12:00:50 +0900

    [update]FirebaseFirestoreのインスタンス生成

--- lib/firestore_api.dart
+++ lib/firestore_api.dart
@@ -1,5 +1,15 @@
+/**
+ * FirestoreApの関数を集約するファイル
+ * 
+ * @author 佐藤佑哉
+ */
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import '../data/question_data.dart';
 
-Future<String> getSubjects() async {
-  return ""; //作成途中
+FirebaseFirestore firestore = FirebaseFirestore.instance;
+
+class FirestoreApi {
+  CollectionReference user = firestore.collection("");
 }
