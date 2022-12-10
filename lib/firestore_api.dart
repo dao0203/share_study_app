885d815bc44e2cba68a1cc5d5cbf27b6ee725bd8
dao0203 <youzaizuoteng872@gmail.com>
2022-12-10 11:30:20 +0900

    [update]API集約クラス：質問投稿のメソッドを追加

--- lib/firestore_api.dart
+++ lib/firestore_api.dart
@@ -11,5 +11,24 @@ import '../data/question_data.dart';
 FirebaseFirestore firestore = FirebaseFirestore.instance;
 
 class FirestoreApi {
-  CollectionReference user = firestore.collection("");
+  /*コレクションリファーレンスを追加*/
+  CollectionReference answers = firestore.collection("answers");
+  CollectionReference questions = firestore.collection("questions");
+  CollectionReference subjects = firestore.collection("subjects");
+
+  // Future<List<String>> fetchAnswer_browse(String question_Id)async{
+
+  // }
+
+  /**
+   * 質問投稿メソッド
+   */
+  Future<void> addQuestion(QuestionData questionData) async =>
+      await questions.add({
+        "title": questionData.titleContent,
+        "text_content": questionData.titleContent,
+        "sub_name": questionData.qSubId,
+        "quesitiom_id": questionData.qId,
+        "email": questionData.email
+      });
 }
