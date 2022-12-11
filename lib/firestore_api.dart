26821473d7cd8a3c8ac802c0a2990e3e420f4f35
dao0203 <youzaizuoteng872@gmail.com>
2022-12-11 13:54:13 +0900

    [update]apiたたく関数（質問POSTメソッド）

--- lib/firestore_api.dart
+++ lib/firestore_api.dart
@@ -3,32 +3,63 @@
  * 
  * @author 佐藤佑哉
  */
-import 'package:firebase_auth/firebase_auth.dart';
+import 'dart:async';
+import 'dart:convert';
 import 'package:flutter/material.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
 import '../data/question_data.dart';
+import 'package:http/http.dart' as http;
 
-FirebaseFirestore firestore = FirebaseFirestore.instance;
+class ApiResults {
+  final String message;
+  ApiResults({
+    required this.message,
+  });
+  factory ApiResults.fromJson(Map<String, dynamic> json) {
+    return ApiResults(
+      message: json['message'],
+    );
+  }
+}
+
+class QuestionPostRequest {
+  final String title;
+  final String content;
+  final String sub_name;
+  final String email;
+  QuestionPostRequest({
+    required this.title,
+    required this.content,
+    required this.sub_name,
+    required this.email,
+  });
+  Map<String, dynamic> toJson() => {
+        'title': title,
+        'sub_name': sub_name,
+        'content': content,
+        'email': email,
+      };
+}
 
 class FirestoreApi {
-  /*コレクションリファーレンスを追加*/
-  CollectionReference answers = firestore.collection("answers");
-  CollectionReference questions = firestore.collection("questions");
-  CollectionReference subjects = firestore.collection("subjects");
-
-  // Future<List<String>> fetchAnswer_browse(String question_Id)async{
-
-  // }
+  /**
+   * 質問GETメソッド
+   */
 
   /**
-   * 質問投稿メソッド
+   * 質問POSTメソッド
    */
-  Future<void> addQuestion(QuestionData questionData) async =>
-      await questions.add({
-        "title": questionData.titleContent,
-        "text_content": questionData.titleContent,
-        "sub_name": questionData.qSubId,
-        "quesitiom_id": questionData.qId,
-        "email": questionData.email
-      });
+  Future<ApiResults> fetchApiResults() async {
+    var url =
+        "https://asia-northeast1-sharestud.cloudfunctions.net/add_question";
+    var request = new QuestionPostRequest(
+        title: "", content: '', sub_name: '', email: '');
+    final response = await http.post(Uri.parse(url),
+        body: json.encode(request.toJson()),
+        headers: {"Content-Type": "application/json"});
+    if (response.statusCode == 200) {
+      return ApiResults.fromJson(json.decode(response.body));
+    } else {
+      throw Exception('Failed');
+    }
+  }
 }
