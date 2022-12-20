821c9bcba8f4484fbd4affdfd019ec123d171e93
dao0203 <youzaizuoteng872@gmail.com>
2022-12-21 00:56:51 +0900

    [add]freezed解答閲覧

--- lib/firestore_api.dart
+++ lib/firestore_api.dart
@@ -4,7 +4,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import '../data/question_data.dart';
+import 'data/question_post_data.dart';
 
 FirebaseFirestore firestore = FirebaseFirestore.instance;
 
