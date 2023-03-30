9555d2e02e429ef77824726eadd545aa27e81d53
佐藤佑哉 <yuyudevelop0203@gmail.com>
2023-03-30 21:14:43 +0900

    [add] add constant file to resolved harded code

---
+++ lib/constants.dart
@@ -0,0 +1,20 @@
+//クエッションコレクションのパラメータ
+const QUESTION_ANSWER_IDS = "answerIds";
+const QUESTION_GOOGLE_ACCOUNT_ID = "googleAccountId";
+const QUESTION_TITLE = "title";
+const QUESTION_SUBJECT_NAME = "subjectName";
+const QUESTION_QUESTION_CONTENT = "textContent";
+const QUESTION_CREATED_AT = "createdAt";
+
+//アンサーコレクションのパラメータ
+const ANSWER_TEXT = "answerText";
+const ANSWER_CREATED_AT = "createdAt";
+const ANSWER_GOOGLE_ACCOUNT_ID = "googleAccountId";
+const ANSWER_QUETSION_ID = "questionId";
+
+//サブジェクトコレクションのパラメータ
+const SUBJECTS_NAME = "subejctName";
+
+//ユーザコレクションのパラメータ
+const USERS_GOOGLE_ACCOUNT_ID = "googleAccountId";
+const USERS_NAME = "name";
