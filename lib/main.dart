0175922a6df8c106eccd4333043f7542337817db
佐藤佑哉 <yuyudevelop0203@gmail.com>
2023-04-03 16:26:34 +0900

    [update] choose page if login or not login

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'dart:developer';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:share_study_app/google_sign_in_demo.dart';
-import 'package:share_study_app/questions_list/thread_page.dart';
+import 'package:share_study_app/view/login_and_registration/login_page.dart';
+import 'package:share_study_app/view/questions_list/thread_page.dart';
+import 'package:share_study_app/view/top_page/title_page.dart';
 import 'firebase_options.dart';
 
 Future<void> main() async {
@@ -14,26 +16,42 @@ Future<void> main() async {
     log("errorを検出：$error");
   });
 
-  await dotenv.load(fileName: '.env');
+  //エミュレータのアドレスとポートを指定
+  // await FirebaseAuth.instance.useAuthEmulator("localhost", 9099);
+
+  // await dotenv.load(fileName: '.env');
 
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
+  static final FirebaseAuth _auth = FirebaseAuth.instance;
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-  
     return MaterialApp(
       routes: {
-        "/home": (context) => const ThreadPage(),
+        ThreadPage.tag: (context) => const ThreadPage(),
+        LoginPage.tag: (context) => const LoginPage(),
       },
       title: 'Share_study_app',
       theme: ThemeData.light(),
       darkTheme: ThemeData.dark(),
-      home: const SignInDemo(),
+      home: StreamBuilder<User?>(
+        stream: _auth.authStateChanges(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const CircularProgressIndicator();
+          } else {
+            if (snapshot.hasData) {
+              return const ThreadPage();
+            } else {
+              return const TitlePage();
+            }
+          }
+        },
+      ),
     );
   }
 }
