93c39cde8e90e58bba455a67024f246709c1cf3f
佐藤佑哉 <yuyudevelop0203@gmail.com>
2023-04-07 00:53:44 +0900

    [add] privacy policy web page

---
+++ lib/view/login_and_registration/privacy_policy_web.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class PrivacyPolicyWebPage extends StatefulWidget {
+  const PrivacyPolicyWebPage({super.key});
+  static String tag = "privacy_policy_web";
+  @override
+  State<PrivacyPolicyWebPage> createState() => _PrivacyPolicyWebPageState();
+}
+
+class _PrivacyPolicyWebPageState extends State<PrivacyPolicyWebPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
