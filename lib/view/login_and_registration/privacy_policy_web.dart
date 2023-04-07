37a213eaa47a5a05ade39f9fc9d01dc0bbd6cc37
佐藤佑哉 <yuyudevelop0203@gmail.com>
2023-04-07 10:15:24 +0900

    [update] add WebViewWiedget

--- lib/view/login_and_registration/privacy_policy_web.dart
+++ lib/view/login_and_registration/privacy_policy_web.dart
@@ -1,5 +1,7 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
 class PrivacyPolicyWebPage extends StatefulWidget {
   const PrivacyPolicyWebPage({super.key});
@@ -11,6 +13,25 @@ class PrivacyPolicyWebPage extends StatefulWidget {
 class _PrivacyPolicyWebPageState extends State<PrivacyPolicyWebPage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final controller = WebViewController()
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..setBackgroundColor(Theme.of(context).scaffoldBackgroundColor)
+      ..setNavigationDelegate(NavigationDelegate(
+        onNavigationRequest: (NavigationRequest request) {
+          if (request.url
+              .startsWith('https://yuyudevelop0203.wixsite.com/my-site')) {
+            return NavigationDecision.prevent;
+          }
+          return NavigationDecision.navigate;
+        },
+      ))
+      ..loadRequest(Uri.parse('https://yuyudevelop0203.wixsite.com/my-site'));
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('プライバシーポリシー'),
+      ),
+      body: WebViewWidget(controller: controller),
+    );
   }
 }
