c9ef4d49b25f1195cecaab0f590a19ea94f20847
佐藤 佑哉 <y-sato@sstn.jp>
2023-08-28 21:41:11 +0900

    implement answer repostiroy class  as firestore function

--- lib/data/repository/firestore_answer_repository.dart
+++ lib/data/repository/firestore_answer_repository.dart
@@ -1,5 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:share_study_app/data/domain/answer.dart';
+import 'package:share_study_app/data/domain/answerer.dart';
+import 'package:share_study_app/data/domain/subject.dart';
 import 'package:share_study_app/data/repository/answer_repositroy.dart';
 
 final class FirestoreAnswerRepository implements AnswerRepository {
@@ -9,12 +11,48 @@ final class FirestoreAnswerRepository implements AnswerRepository {
 
   @override
   Future<void> addAnswer(Answer answer) async {
-    await _answersCollection.add({});
+    await _answersCollection.add({
+      'questionId': answer.questionId,
+      'content': answer.content,
+      'imageUrl': answer.imageUrl,
+      'isBestAnswer': false,
+      'subject': {
+        'id': answer.subject.id,
+        'name': answer.subject.name,
+        'course': answer.subject.course,
+      },
+      'createdAt': Timestamp.now(),
+      'updatedAt': Timestamp.now(),
+    });
   }
 
   @override
-  Future<List<Answer>> getAnswers() {
-    // TODO: implement getAnswers
-    throw UnimplementedError();
+  Future<List<Answer>> getAnswers() async {
+    return await _answersCollection.get().then(
+          (value) => value.docs
+              .map(
+                (e) => Answer(
+                  id: e.id,
+                  questionId: e['questionId'],
+                  content: e['content'],
+                  imageUrl: e['imageUrl'],
+                  isBestAnswer: e['isBestAnswer'],
+                  answerer: Answerer(
+                    id: e['answerer']['id'],
+                    lastName: e['answerer']['lastName'],
+                    firstName: e['answerer']['firstName'],
+                    imageUrl: e['answerer']['imageUrl'],
+                  ),
+                  subject: Subject(
+                    id: e['subject']['id'],
+                    name: e['subject']['name'],
+                    course: e['subject']['course'],
+                  ),
+                  createdAt: e['createdAt'].toDate(),
+                  updatedAt: e['updatedAt'].toDate(),
+                ),
+              )
+              .toList(),
+        );
   }
 }
