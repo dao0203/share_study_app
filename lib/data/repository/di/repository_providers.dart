f8f82f47bb5ece717ba37fb6e09034ac69e34a3a
佐藤 佑哉 <y-sato@sstn.jp>
2023-08-28 22:07:21 +0900

    add repository providers file

---
+++ lib/data/repository/di/repository_providers.dart
@@ -0,0 +1,23 @@
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:share_study_app/data/firebase/firestore_providers.dart';
+import 'package:share_study_app/data/repository/answer_repositroy.dart';
+import 'package:share_study_app/data/repository/firestore_answer_repository.dart';
+import 'package:share_study_app/data/repository/firestore_question_respotiroy.dart';
+import 'package:share_study_app/data/repository/firestore_subject_repository.dart';
+import 'package:share_study_app/data/repository/question_repostiroy.dart';
+import 'package:share_study_app/data/repository/subject_repository.dart';
+
+final answerRepositoryProvider = Provider<AnswerRepository>((ref) {
+  final answerCollectoinRef = ref.watch(answersCollectionRefProvider);
+  return FirestoreAnswerRepository(answerCollectoinRef);
+});
+
+final questionRepositoryProvider = Provider<QuestionRepository>((ref) {
+  final questionCollectoinRef = ref.watch(questionsCollectionRefProvider);
+  return FirestoreQuestionRepository(questionCollectoinRef);
+});
+
+final subjectRepositoryProvider = Provider<SubjectRepository>((ref) {
+  final subjectCollectoinRef = ref.watch(subjectsCollectionRefProvider);
+  return FirestoreSubjectRepository(subjectCollectoinRef);
+});
