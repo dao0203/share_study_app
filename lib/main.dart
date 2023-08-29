86ebcd636265bd7e248e8fc128539cca5b9c4a6a
佐藤 佑哉 <y-sato@sstn.jp>
2023-08-29 14:06:56 +0900

    wrap provider scope to use provider

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,10 @@ import 'dart:developer';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:share_study_app/ui/theme/colors.dart';
 import 'package:share_study_app/ui/view/onboarding/login/login_page.dart';
-import 'package:share_study_app/ui/view/common/privacy_policy_web.dart';
+import 'package:share_study_app/ui/view/privacy_policy/privacy_policy_web.dart';
 import 'package:share_study_app/ui/view/onboarding/registration/register_page.dart';
 import 'package:share_study_app/ui/view/timeline/thread_page.dart';
 import 'firebase_options.dart';
@@ -28,36 +29,38 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      routes: {
-        ThreadPage.tag: (context) => const ThreadPage(),
-        LoginPage.tag: (context) => const LoginPage(),
-        RegisterPage.tag: (context) => const RegisterPage(),
-        PrivacyPolicyWebPage.tag: (context) => const PrivacyPolicyWebPage(),
-      },
-      title: 'Share_study_app',
-      theme: ThemeData(
-        useMaterial3: true,
-        colorScheme: lightColorScheme,
-      ),
-      darkTheme: ThemeData(
-        useMaterial3: true,
-        colorScheme: darkColorScheme,
-      ),
-      home: StreamBuilder<User?>(
-        stream: _auth.authStateChanges(),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const CircularProgressIndicator();
-          } else {
-            if (snapshot.hasData) {
-              return const ThreadPage();
-            } else {
-              return const LoginPage();
-            }
-          }
+    return ProviderScope(
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        routes: {
+          "questions_screen": (context) => const QuestionsScreen(),
+          LoginPage.tag: (context) => const LoginPage(),
+          RegisterPage.tag: (context) => const RegisterPage(),
+          PrivacyPolicyWebPage.tag: (context) => const PrivacyPolicyWebPage(),
         },
+        title: 'Share_study_app',
+        theme: ThemeData(
+          useMaterial3: true,
+          colorScheme: lightColorScheme,
+        ),
+        darkTheme: ThemeData(
+          useMaterial3: true,
+          colorScheme: darkColorScheme,
+        ),
+        home: StreamBuilder<User?>(
+          stream: _auth.authStateChanges(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const CircularProgressIndicator();
+            } else {
+              if (snapshot.hasData) {
+                return const QuestionsScreen();
+              } else {
+                return const LoginPage();
+              }
+            }
+          },
+        ),
       ),
     );
   }
