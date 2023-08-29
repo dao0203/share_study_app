949bf8ac3dfd9d7785d11a0d892306db9a3a0cda
佐藤 佑哉 <y-sato@sstn.jp>
2023-08-29 19:47:45 +0900

    implement user auth repository as supabase

---
+++ lib/data/repository/supabase_user_auth_repository.dart
@@ -0,0 +1,26 @@
+import 'package:share_study_app/data/repository/user_auth_repository.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+final class SupabaseUserAuthRepository implements UserAuthRepository {
+  @override
+  bool isUserLoggedIn() {
+    return Supabase.instance.client.auth.currentSession != null;
+  }
+
+  @override
+  Future<void> signOut() async {
+    await Supabase.instance.client.auth.signOut();
+  }
+
+  @override
+  Future<void> signIn(String email, String password) async {
+    await Supabase.instance.client.auth
+        .signInWithPassword(email: email, password: password);
+  }
+
+  @override
+  Future<void> signUp(String email, String password) async {
+    await Supabase.instance.client.auth
+        .signUp(email: email, password: password);
+  }
+}
