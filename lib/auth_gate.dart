53b5a66b1cbe9105bce310b50fbf5ee3b8ab8016
佐藤 佑哉 <y-sato@sstn.jp>
2023-08-29 20:17:47 +0900

    change auth gate

--- lib/auth_gate.dart
+++ lib/auth_gate.dart
@@ -1,14 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:share_study_app/data/repository/di/repository_providers.dart';
 import 'package:share_study_app/ui/view/onboarding/login/login_page.dart';
 import 'package:share_study_app/ui/view/timeline/thread_page.dart';
-import 'package:supabase_flutter/supabase_flutter.dart';
 
 class AuthGate extends HookConsumerWidget {
   const AuthGate({super.key});
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return Supabase.instance.client.auth.currentSession != null
+    final userAuthRepository = ref.watch(userAuthRepositoryProvider);
+    return userAuthRepository.isUserSignedIn() == true
         ? const QuestionsScreen()
         : const LoginPage();
   }
