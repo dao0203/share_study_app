31b1a23b258a033b6954834dcdf6d6196e6ff53e
佐藤 佑哉 <y-sato@sstn.jp>
2023-08-29 20:18:00 +0900

    delete unused import file

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,8 @@
-import 'dart:developer';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:share_study_app/share_study_app.dart';
-import 'package:share_study_app/ui/theme/colors.dart';
-import 'package:share_study_app/ui/view/onboarding/login/login_page.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'firebase_options.dart';
 
