ea869a99b986cbca6762f8922ca2377a2076b2e8
佐藤 佑哉 <y-sato@sstn.jp>
2023-08-30 20:14:49 +0900

    set supabase sign up function

--- lib/ui/view/onboarding/sign_up/sign_up_screen.dart
+++ lib/ui/view/onboarding/sign_up/sign_up_screen.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:logger/logger.dart';
+import 'package:share_study_app/data/repository/di/repository_providers.dart';
+import 'package:share_study_app/ui/view/onboarding/registration_profile/registration_profile_screen.dart';
 
 class SignUpScreen extends HookConsumerWidget {
   const SignUpScreen({super.key});
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final userRepository = ref.watch(userRepositoryProvider);
     final emailController = useTextEditingController();
     final passwordController = useTextEditingController();
     final passwordConfirmController = useTextEditingController();
@@ -113,8 +117,33 @@ class SignUpScreen extends HookConsumerWidget {
                         }),
                     const SizedBox(height: 30),
                     ElevatedButton.icon(
-                      onPressed: () {
-                        if (_formKey.currentState!.validate()) {}
+                      onPressed: () async {
+                        if (_formKey.currentState!.validate()) {
+                          showDialog(
+                              context: context,
+                              barrierDismissible: false,
+                              builder: (_) => const Center(
+                                  child: CircularProgressIndicator()));
+                          await userRepository
+                              .signUp(emailController.text,
+                                  passwordConfirmController.text)
+                              .then((value) {
+                            Logger().d("signup success");
+                            Navigator.of(context).push(
+                              MaterialPageRoute(
+                                builder: (context) =>
+                                    const RegistrationProfileScreen(),
+                              ),
+                            );
+                          }).catchError((e) {
+                            Logger().e(e);
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              SnackBar(
+                                content: Text(e.toString()),
+                              ),
+                            );
+                          });
+                        }
                       },
                       icon: const Icon(Icons.person_add),
                       label: const Text("サインアップ"),
