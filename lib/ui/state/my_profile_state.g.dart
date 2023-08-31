44150ade63cbff3cb8d9068bf82c8bcac690efbd
佐藤 佑哉 <y-sato@sstn.jp>
2023-08-31 21:43:28 +0900

    add my profile state

---
+++ lib/ui/state/my_profile_state.g.dart
@@ -0,0 +1,26 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'my_profile_state.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$myProfileStateHash() => r'96c73e82c82594f8bf1e95c24f50bef487ac3cba';
+
+/// See also [MyProfileState].
+@ProviderFor(MyProfileState)
+final myProfileStateProvider =
+    AutoDisposeAsyncNotifierProvider<MyProfileState, Profile>.internal(
+  MyProfileState.new,
+  name: r'myProfileStateProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$myProfileStateHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$MyProfileState = AutoDisposeAsyncNotifier<Profile>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member
