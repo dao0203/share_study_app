2e5762819160b5d8615a93144fbf175324a35b0f
佐藤 佑哉 <y-sato@sstn.jp>
2023-09-01 12:25:40 +0900

    change provider name

--- lib/auth_gate.dart
+++ lib/auth_gate.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:share_study_app/data/repository/di/repository_providers.dart';
+import 'package:share_study_app/ui/view/onboarding/registration_profile/registration_profile_screen.dart';
 import 'package:share_study_app/ui/view/onboarding/sign_in/sign_in_screen.dart';
 import 'package:share_study_app/ui/view/timeline/thread_page.dart';
 
@@ -8,9 +9,9 @@ class AuthGate extends HookConsumerWidget {
   const AuthGate({super.key});
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final userAuthRepository = ref.watch(userRepositoryProvider);
+    final userAuthRepository = ref.watch(userAuthRepositoryProvider);
     return userAuthRepository.isUserSignedIn() == true
-        ? const QuestionsScreen()
+        ? RegistrationProfileScreen()
         : const SignInScreen();
   }
 }
