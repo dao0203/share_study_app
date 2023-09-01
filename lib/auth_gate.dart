f6dabcffd3868c7287e1641b82c0a2e0b99e665c
佐藤 佑哉 <y-sato@sstn.jp>
2023-09-01 13:14:10 +0900

    add error handling to auth gate

--- lib/auth_gate.dart
+++ lib/auth_gate.dart
@@ -1,17 +1,52 @@
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:share_study_app/data/repository/di/repository_providers.dart';
+import 'package:logger/logger.dart';
+import 'package:share_study_app/ui/state/splash_state.dart';
 import 'package:share_study_app/ui/view/onboarding/registration_profile/registration_profile_screen.dart';
 import 'package:share_study_app/ui/view/onboarding/sign_in/sign_in_screen.dart';
 import 'package:share_study_app/ui/view/timeline/thread_page.dart';
+import 'package:share_study_app/util/splash.dart';
 
 class AuthGate extends HookConsumerWidget {
   const AuthGate({super.key});
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final userAuthRepository = ref.watch(userAuthRepositoryProvider);
-    return userAuthRepository.isUserSignedIn() == true
-        ? RegistrationProfileScreen()
-        : const SignInScreen();
+    final splashState = ref.watch(splashStateProvider);
+    return splashState.when(
+        data: (splashTo) {
+          switch (splashTo) {
+            case SplashTo.signInScreen:
+              return const SignInScreen();
+            case SplashTo.profileScreen:
+              return RegistrationProfileScreen();
+            case SplashTo.homeScreen:
+              return const QuestionsScreen();
+          }
+        },
+        loading: () => const Scaffold(
+              body: Center(
+                child: CircularProgressIndicator(),
+              ),
+            ),
+        error: (error, stacktrace) {
+          Logger().e({error, stacktrace});
+          return Scaffold(
+            body: Center(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text("エラーが発生しました"),
+                  TextButton(
+                    onPressed: () {
+                      ref.refresh(splashStateProvider);
+                    },
+                    child: const Text('再試行'),
+                  ),
+                ],
+              ),
+            ),
+          );
+        });
   }
 }
