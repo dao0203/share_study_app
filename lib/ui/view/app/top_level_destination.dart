3c247b7635debe94d8449147d57b573a0cf65d80
佐藤 佑哉 <y-sato@sstn.jp>
2023-09-01 14:12:23 +0900

    add enum top level destination

---
+++ lib/ui/view/app/top_level_destination.dart
@@ -0,0 +1,21 @@
+
+import 'package:flutter/material.dart';
+
+enum TopLevelDestination{
+  
+  const TopLevelDestination({
+    required this.title,
+    required this.icon,
+    required this.page,
+  });
+  final String title;
+  final IconData icon;
+  final Widget page;
+
+  timeline(
+    title: 'タイムライン',
+    icon: Icons.home,
+    page: const QuestionsScreen(),
+  ),
+
+}
