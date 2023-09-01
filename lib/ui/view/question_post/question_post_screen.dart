fa88b5b94fe00704a0ea46bff605d57b3c2ec37d
佐藤 佑哉 <y-sato@sstn.jp>
2023-09-02 00:35:03 +0900

    update question post screen state

--- lib/ui/view/question_post/question_post_screen.dart
+++ lib/ui/view/question_post/question_post_screen.dart
@@ -1,23 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:logger/logger.dart';
 
-class QuestionPostScreen extends StatefulHookConsumerWidget {
+class QuestionPostScreen extends HookConsumerWidget {
   const QuestionPostScreen({super.key});
 
   @override
-  ConsumerState<ConsumerStatefulWidget> createState() =>
-      _QuestionPostScreenState();
-}
+  Widget build(BuildContext context, WidgetRef ref) {
+    Logger().d('QuestionPostScreen build');
+    final titleController = useTextEditingController(text: '');
+    final subjectController = useTextEditingController(text: '');
+    final contentController = useTextEditingController(text: '');
+    final areFieldEmpty = useState(true);
 
-class _QuestionPostScreenState extends ConsumerState<QuestionPostScreen> {
-  @override
-  Widget build(BuildContext context) {
+    bool checkIfFieldsAreEmpty() {
+      return titleController.text.isEmpty ||
+          subjectController.text.isEmpty ||
+          contentController.text.isEmpty;
+    }
+
+    useEffect(() {
+      Logger().d('QuestionPostScreen useEffect');
+      titleController.addListener(() {
+        areFieldEmpty.value = checkIfFieldsAreEmpty();
+      });
+      subjectController.addListener(() {
+        areFieldEmpty.value = checkIfFieldsAreEmpty();
+      });
+      contentController.addListener(() {
+        areFieldEmpty.value = checkIfFieldsAreEmpty();
+      });
+      return null;
+    }, [titleController, subjectController, contentController]);
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.background,
         actions: [
-          TextButton.icon(
-            onPressed: () {},
+          ElevatedButton.icon(
+            onPressed: areFieldEmpty.value ? null : () {},
             icon: Icon(
               Icons.send,
               color: Theme.of(context).colorScheme.onBackground,
@@ -29,27 +50,43 @@ class _QuestionPostScreenState extends ConsumerState<QuestionPostScreen> {
               ),
             ),
           ),
+          const SizedBox(width: 10)
         ],
       ),
       //タイトルと科目と本文の入力
-      body: Column(
-        children: [
-          TextField(
-            decoration: InputDecoration(
-              hintText: 'タイトル',
-            ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              TextField(
+                controller: titleController,
+                maxLength: 30,
+                decoration: const InputDecoration(
+                  labelText: 'タイトル',
+                  prefixIcon: Icon(Icons.title),
+                ),
+              ),
+              TextField(
+                controller: subjectController,
+                maxLength: 20,
+                decoration: const InputDecoration(
+                  labelText: '科目',
+                  prefixIcon: Icon(Icons.menu_book),
+                ),
+              ),
+              TextField(
+                controller: contentController,
+                maxLength: 200,
+                maxLines: 6,
+                decoration: const InputDecoration(
+                  labelText: '本文',
+                  prefixIcon: Icon(Icons.subject),
+                ),
+              ),
+            ],
           ),
-          TextField(
-            decoration: InputDecoration(
-              hintText: '科目',
-            ),
-          ),
-          TextField(
-            decoration: InputDecoration(
-              hintText: '本文',
-            ),
-          ),
-        ],
+        ),
       ),
     );
   }
