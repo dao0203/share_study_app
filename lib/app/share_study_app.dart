a216ca2136acb46717b973b66a98bb57adf58366
dao0203 <youzaizuoteng872@gmail.com>
2023-09-21 01:52:21 +0900

    警告を削除

--- lib/app/share_study_app.dart
+++ lib/app/share_study_app.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:share_study_app/ui/state/my_profile_state.dart';
 import 'package:share_study_app/app/top_level_destination.dart';
 import 'package:share_study_app/ui/view/question_post/question_post_screen.dart';
 
