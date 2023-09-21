c68010d525d90966f6034120ef1b60652506b94c
dao0203 <youzaizuoteng872@gmail.com>
2023-09-22 01:19:37 +0900

    delete unused file

--- lib/app/share_study_app.dart
+++ lib/app/share_study_app.dart
@@ -2,8 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:share_study_app/app/top_level_destination.dart';
-import 'package:share_study_app/ui/components/question_post_fab.dart';
-import 'package:share_study_app/ui/view/question_post/question_post_screen.dart';
 
 final _navigatorKey = <TopLevelDestination, GlobalKey<NavigatorState>>{
   for (final item in TopLevelDestination.values)
