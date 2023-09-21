4bd8cbf64eb35383332ed9a1bf26b0586a1d389d
dao0203 <youzaizuoteng872@gmail.com>
2023-09-22 03:02:22 +0900

    下に回答記入用のTextFieldを設置

--- lib/ui/view/discussion/discussion_screen.dart
+++ lib/ui/view/discussion/discussion_screen.dart
@@ -21,77 +21,102 @@ class _DiscussionScreenState extends ConsumerState<DiscussionScreen> {
       ),
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 16),
-        child: SingleChildScrollView(
-          child: Column(
-            children: [
-              Text(
-                quesiton.title,
-                style: const TextStyle(
-                  fontSize: 28,
-                  fontWeight: FontWeight.bold,
-                ),
-                maxLines: 3,
-                overflow: TextOverflow.ellipsis,
-              ),
-              const SizedBox(height: 16),
-              Card(
-                color: Theme.of(context).colorScheme.secondaryContainer,
-                elevation: 3,
-                child: Row(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: quesiton.questioner.imageUrl != null
-                          ? CircleAvatar(
-                              backgroundImage:
-                                  NetworkImage(quesiton.questioner.imageUrl!),
-                              radius: 40,
-                            )
-                          : Icon(
-                              Icons.person_outline_outlined,
-                              color: Theme.of(context)
-                                  .colorScheme
-                                  .onSecondaryContainer,
-                              size: 40,
-                            ),
+        child: Column(
+          children: [
+            Expanded(
+              child: Column(
+                children: [
+                  Text(
+                    quesiton.title,
+                    style: const TextStyle(
+                      fontSize: 28,
+                      fontWeight: FontWeight.bold,
                     ),
-                    Expanded(
-                      child: Padding(
-                        padding: const EdgeInsets.only(bottom: 8, right: 8),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              quesiton.questioner.nickname,
-                              style: TextStyle(
-                                fontSize: 16,
-                                fontWeight: FontWeight.bold,
-                                color:
-                                    Theme.of(context).colorScheme.onBackground,
-                                letterSpacing: 2,
-                              ),
-                            ),
-                            const SizedBox(width: 8),
-                            Text(
-                              quesiton.content,
-                              style: TextStyle(
-                                fontSize: 16,
-                                color:
-                                    Theme.of(context).colorScheme.onBackground,
-                                letterSpacing: 2,
-                              ),
-                            ),
-                          ],
+                    maxLines: 3,
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                  const SizedBox(height: 16),
+                  Card(
+                    color: Theme.of(context).colorScheme.secondaryContainer,
+                    elevation: 3,
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: quesiton.questioner.imageUrl != null
+                              ? CircleAvatar(
+                                  backgroundImage: NetworkImage(
+                                      quesiton.questioner.imageUrl!),
+                                  radius: 40,
+                                )
+                              : Icon(
+                                  Icons.person_outline_outlined,
+                                  color: Theme.of(context)
+                                      .colorScheme
+                                      .onSecondaryContainer,
+                                  size: 40,
+                                ),
                         ),
+                        Expanded(
+                          child: Padding(
+                            padding: const EdgeInsets.only(bottom: 8, right: 8),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  quesiton.questioner.nickname,
+                                  style: TextStyle(
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.bold,
+                                    color: Theme.of(context)
+                                        .colorScheme
+                                        .onBackground,
+                                    letterSpacing: 2,
+                                  ),
+                                ),
+                                const SizedBox(width: 8),
+                                Text(
+                                  quesiton.content,
+                                  style: TextStyle(
+                                    fontSize: 16,
+                                    color: Theme.of(context)
+                                        .colorScheme
+                                        .onBackground,
+                                    letterSpacing: 2,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(height: 16),
+                ],
+              ),
+            ),
+            Row(
+              children: [
+                Expanded(
+                  child: TextField(
+                    decoration: InputDecoration(
+                      hintText: 'コメントを入力',
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(100),
                       ),
                     ),
-                  ],
+                    style: TextStyle(
+                      fontSize: 16,
+                      color: Theme.of(context).colorScheme.onBackground,
+                      letterSpacing: 1.5,
+                    ),
+                  ),
                 ),
-              ),
-              const SizedBox(height: 16),
-            ],
-          ),
+              ],
+            ),
+          ],
         ),
       ),
     );
