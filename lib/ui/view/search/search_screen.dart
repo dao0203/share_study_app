16c4f294fa7c3f854e0527603392f3193fd212b9
dao0203 <youzaizuoteng872@gmail.com>
2023-09-22 17:42:52 +0900

    検索していない時のWidgetを実装

--- lib/ui/view/search/search_screen.dart
+++ lib/ui/view/search/search_screen.dart
@@ -3,14 +3,31 @@ import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:share_study_app/app/share_study_drawer.dart';
 
-class SearchScreen extends HookConsumerWidget {
+class SearchScreen extends StatefulHookConsumerWidget {
   const SearchScreen({super.key});
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  ConsumerState<SearchScreen> createState() => _SearchScreenState();
+}
+
+class _SearchScreenState extends ConsumerState<SearchScreen> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
     final scaffoldKey = useState(GlobalKey<ScaffoldState>());
     final searchController = useTextEditingController();
+    final isSearchTextFieldEmpty = useState(true);
+    final isSearching = useState(false);
+    searchController.addListener(() {
+      isSearchTextFieldEmpty.value = searchController.text.isEmpty;
+    });
+
     return Scaffold(
       key: scaffoldKey.value,
+      resizeToAvoidBottomInset: false,
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.background,
         leading: IconButton(
@@ -22,7 +39,11 @@ class SearchScreen extends HookConsumerWidget {
           decoration: InputDecoration(
             contentPadding: const EdgeInsets.all(8),
             hintText: '検索',
-            border: InputBorder.none,
+            //丸くする
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(20),
+              borderSide: BorderSide.none,
+            ),
             suffixIcon: const Icon(Icons.search),
             filled: true,
             fillColor: Theme.of(context).colorScheme.surface,
@@ -31,9 +52,28 @@ class SearchScreen extends HookConsumerWidget {
         centerTitle: true,
       ),
       drawer: const ShareStudyDrawer(),
-      body: const Center(
-        child: Text('検索'),
-      ),
+      body: isSearching.value
+          ? const Center(
+              child: CircularProgressIndicator(),
+            )
+          : Center(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(
+                    Icons.search,
+                    size: 100,
+                    color: Theme.of(context).colorScheme.onSurface,
+                  ),
+                  const SizedBox(height: 16),
+                  Text(
+                    '検索をするとここに結果が表示されます',
+                    style: Theme.of(context).textTheme.bodyMedium,
+                  ),
+                ],
+              ),
+            ),
     );
   }
 }
