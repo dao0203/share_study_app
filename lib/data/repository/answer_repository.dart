c8f33fc8de853b36fc163dfd6171764a8a069114
dao0203 <youzaizuoteng872@gmail.com>
2023-09-22 12:22:28 +0900

    update interface answer repository

--- lib/data/repository/answer_repository.dart
+++ lib/data/repository/answer_repository.dart
@@ -2,5 +2,7 @@ import 'package:share_study_app/data/domain/answer.dart';
 
 abstract interface class AnswerRepository {
   Future<List<Answer>> getAnswers();
-  Future<void> addAnswer(Answer answer);
+  Future<void> addAnswer(String questionId, String content);
+  Future<List<Answer>> getAnswersByQuestionIdWithPagination(
+      String questionId, int start, int end);
 }
