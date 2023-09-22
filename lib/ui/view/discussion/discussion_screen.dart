5f624086a19a9311dbb844b3ac1ab1b88c10fd87
dao0203 <youzaizuoteng872@gmail.com>
2023-09-22 13:22:07 +0900

    トーク画面のコメントを実装

--- lib/ui/view/discussion/discussion_screen.dart
+++ lib/ui/view/discussion/discussion_screen.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:flutter_staggered_animations/flutter_staggered_animations.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:infinite_scroll_pagination/infinite_scroll_pagination.dart';
+import 'package:logger/logger.dart';
+import 'package:share_study_app/data/domain/answer.dart';
 import 'package:share_study_app/data/domain/question.dart';
+import 'package:share_study_app/data/repository/di/repository_providers.dart';
+import 'package:share_study_app/ui/view/discussion/components/answer_item.dart';
 
 class DiscussionScreen extends StatefulHookConsumerWidget {
   const DiscussionScreen({super.key, required this.question});
@@ -13,6 +19,37 @@ class DiscussionScreen extends StatefulHookConsumerWidget {
 }
 
 class _DiscussionScreenState extends ConsumerState<DiscussionScreen> {
+  static const _pageSize = 10;
+  final PagingController<int, Answer> _pagingController =
+      PagingController(firstPageKey: 0);
+  @override
+  void initState() {
+    super.initState();
+    _pagingController.addPageRequestListener((pageKey) {
+      Logger().d('pageKey: $pageKey');
+      _fetchPage(pageKey);
+    });
+  }
+
+  Future<void> _fetchPage(int pageKey) async {
+    Logger().d('_fetchPage: $pageKey');
+    try {
+      final newItems = await ref
+          .watch(answerRepositoryProvider)
+          .getAnswersByQuestionIdWithPagination(
+              widget.question.id, pageKey, _pageSize + pageKey);
+      final isLastPage = newItems.length < _pageSize;
+      if (isLastPage) {
+        _pagingController.appendLastPage(newItems);
+      } else {
+        final nextPageKey = pageKey + newItems.length;
+        _pagingController.appendPage(newItems, nextPageKey);
+      }
+    } catch (error) {
+      _pagingController.error = error;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     final question = widget.question;
@@ -37,145 +74,163 @@ class _DiscussionScreenState extends ConsumerState<DiscussionScreen> {
         padding: const EdgeInsets.symmetric(horizontal: 16),
         child: Column(
           children: [
-            Expanded(
-              child: Column(
+            Text(
+              question.title,
+              style: const TextStyle(
+                fontSize: 28,
+                fontWeight: FontWeight.bold,
+              ),
+              maxLines: 3,
+              overflow: TextOverflow.ellipsis,
+            ),
+            const SizedBox(height: 16),
+            Card(
+              color: Theme.of(context).colorScheme.secondaryContainer,
+              elevation: 3,
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Text(
-                    question.title,
-                    style: const TextStyle(
-                      fontSize: 28,
-                      fontWeight: FontWeight.bold,
-                    ),
-                    maxLines: 3,
-                    overflow: TextOverflow.ellipsis,
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: question.questioner.imageUrl != null
+                        ? CircleAvatar(
+                            backgroundImage:
+                                NetworkImage(question.questioner.imageUrl!),
+                            radius: 40,
+                          )
+                        : Icon(
+                            Icons.person_outline_outlined,
+                            color: Theme.of(context)
+                                .colorScheme
+                                .onSecondaryContainer,
+                            size: 40,
+                          ),
                   ),
-                  const SizedBox(height: 16),
-                  Card(
-                    color: Theme.of(context).colorScheme.secondaryContainer,
-                    elevation: 3,
-                    child: Row(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: question.questioner.imageUrl != null
-                              ? CircleAvatar(
-                                  backgroundImage: NetworkImage(
-                                      question.questioner.imageUrl!),
-                                  radius: 40,
-                                )
-                              : Icon(
-                                  Icons.person_outline_outlined,
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.only(bottom: 8, right: 8),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Row(
+                            children: [
+                              Text(
+                                question.questioner.nickname,
+                                style: TextStyle(
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.bold,
                                   color: Theme.of(context)
                                       .colorScheme
-                                      .onSecondaryContainer,
-                                  size: 40,
+                                      .onBackground,
+                                  letterSpacing: 1.5,
                                 ),
-                        ),
-                        Expanded(
-                          child: Padding(
-                            padding: const EdgeInsets.only(bottom: 8, right: 8),
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Row(
-                                  children: [
-                                    Text(
-                                      question.questioner.nickname,
-                                      style: TextStyle(
-                                        fontSize: 16,
-                                        fontWeight: FontWeight.bold,
-                                        color: Theme.of(context)
-                                            .colorScheme
-                                            .onBackground,
-                                        letterSpacing: 1.5,
-                                      ),
+                              ),
+                              const SizedBox(width: 8),
+                              Expanded(
+                                child: Align(
+                                  alignment: Alignment.bottomRight,
+                                  child: Text(
+                                    '${question.createdAt.month}月${question.createdAt.day}日',
+                                    style: TextStyle(
+                                      fontSize: 12,
+                                      color: Theme.of(context)
+                                          .colorScheme
+                                          .onBackground,
+                                      letterSpacing: 1.5,
                                     ),
-                                    const SizedBox(width: 8),
-                                    Expanded(
-                                      child: Align(
-                                        alignment: Alignment.bottomRight,
-                                        child: Text(
-                                          '${question.createdAt.month}月${question.createdAt.day}日',
-                                          style: TextStyle(
-                                            fontSize: 12,
-                                            color: Theme.of(context)
-                                                .colorScheme
-                                                .onBackground,
-                                            letterSpacing: 1.5,
-                                          ),
-                                        ),
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                                const SizedBox(width: 8),
-                                Text(
-                                  question.content,
-                                  style: TextStyle(
-                                    fontSize: 16,
-                                    color: Theme.of(context)
-                                        .colorScheme
-                                        .onBackground,
-                                    letterSpacing: 2,
                                   ),
                                 ),
-                              ],
+                              ),
+                            ],
+                          ),
+                          const SizedBox(width: 8),
+                          Text(
+                            question.content,
+                            style: TextStyle(
+                              fontSize: 16,
+                              color: Theme.of(context).colorScheme.onBackground,
+                              letterSpacing: 2,
                             ),
                           ),
-                        ),
-                      ],
-                    ),
-                  ),
-                  const SizedBox(height: 16),
-                ],
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(bottom: 16),
-              child: Row(
-                children: [
-                  Expanded(
-                    child: TextField(
-                      controller: commentController,
-                      decoration: InputDecoration(
-                        contentPadding: const EdgeInsets.all(16),
-                        hintText: 'コメントを入力',
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(100),
-                        ),
-                        focusedBorder: OutlineInputBorder(
-                          borderSide: BorderSide(
-                            color: Theme.of(context).colorScheme.background,
-                          ),
-                        ),
-                        enabledBorder: OutlineInputBorder(
-                          borderSide: BorderSide(
-                            color: Theme.of(context).colorScheme.background,
-                          ),
-                        ),
-                        filled: true,
-                        fillColor: Colors.grey.withOpacity(0.2),
-                      ),
-                      style: TextStyle(
-                        fontSize: 16,
-                        color: Theme.of(context).colorScheme.onBackground,
-                        letterSpacing: 1.5,
+                        ],
                       ),
                     ),
-                  ),
-                  IconButton(
-                    onPressed: areFieldEmpty.value ? null : () {},
-                    icon: const Icon(Icons.send),
-                    color: areFieldEmpty.value
-                        ? null
-                        : Theme.of(context).colorScheme.surfaceTint,
                   ),
                 ],
               ),
             ),
+            const SizedBox(height: 16),
+            Expanded(
+              child: PagedListView(
+                pagingController: _pagingController,
+                builderDelegate: PagedChildBuilderDelegate<Answer>(
+                  itemBuilder: (context, answer, index) {
+                    return AnimationConfiguration.staggeredList(
+                      position: index,
+                      duration: const Duration(milliseconds: 300),
+                      child: SlideAnimation(
+                        child: FadeInAnimation(
+                          child: AnswerItem(
+                            answer: answer,
+                          ),
+                        ),
+                      ),
+                    );
+                  },
+                ),
+              ),
+            ),
+            const SizedBox(height: 16),
           ],
         ),
+      ),
+      bottomSheet: Row(
+        children: [
+          Expanded(
+            child: TextField(
+              controller: commentController,
+              decoration: InputDecoration(
+                contentPadding: const EdgeInsets.all(16),
+                hintText: 'コメントを入力',
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(100),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Theme.of(context).colorScheme.background,
+                  ),
+                ),
+                enabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Theme.of(context).colorScheme.background,
+                  ),
+                ),
+                filled: true,
+                fillColor: Colors.grey.withOpacity(0.2),
+              ),
+              style: TextStyle(
+                fontSize: 16,
+                color: Theme.of(context).colorScheme.onBackground,
+                letterSpacing: 1.5,
+              ),
+            ),
+          ),
+          IconButton(
+            onPressed: areFieldEmpty.value
+                ? null
+                : () async {
+                    await ref.read(answerRepositoryProvider).addAnswer(
+                          question.id,
+                          commentController.text,
+                        );
+                    commentController.clear();
+                  },
+            icon: const Icon(Icons.send),
+            color: areFieldEmpty.value
+                ? null
+                : Theme.of(context).colorScheme.surfaceTint,
+          ),
+        ],
       ),
     );
   }
