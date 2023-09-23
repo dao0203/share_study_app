8d836de130dd1a04046d428930ac447ea04335e2
dao0203 <youzaizuoteng872@gmail.com>
2023-09-23 18:10:23 +0900

    remove unused repository

--- lib/data/repository/supabase_notification_repository.dart
+++ lib/data/repository/supabase_notification_repository.dart
@@ -6,7 +6,6 @@ import 'package:supabase_flutter/supabase_flutter.dart';
 
 final class SupabaseNotificationRepository implements NotificationRepository {
   final _client = Supabase.instance.client;
-  @override
   Future<List<Notification>> getNotifications(String userId) async {
     return await _client
         .from('notifications')
@@ -39,6 +38,38 @@ final class SupabaseNotificationRepository implements NotificationRepository {
         )
         .catchError((error, stacktrace) {
           Logger().e('getNotifications.error: $error $stacktrace');
+          throw error;
+        });
+  }
+
+  @override
+  Future<List<Notification>> getWithPagination(int start, int end) async {
+    return await _client
+        .from('notifications')
+        .select<PostgrestList>('''
+    id, message, sender_id, question_id, created_at,
+    sender_id (id, nickname,university_name,image_url)
+  ''')
+        .eq('receiver_id', _client.auth.currentUser!.id)
+        .then((value) {
+          Logger().i('getWithPagination.then: $value');
+          return value.map((e) {
+            return Notification(
+              id: e['id'] as String,
+              message: e['message'] as String,
+              sender: Profile(
+                id: e['sender_id']['id'] as String,
+                nickname: e['sender_id']['nickname'] as String,
+                universityName: e['sender_id']['university_name'] as String,
+                imageUrl: e['sender_id']['image_url'] as String?,
+              ),
+              questionId: e['question_id'] as String?,
+              createdAt: DateTime.parse(e['created_at']),
+            );
+          }).toList();
+        })
+        .catchError((error, stacktrace) {
+          Logger().e('getWithPagination.error: $error $stacktrace');
           throw error;
         });
   }
