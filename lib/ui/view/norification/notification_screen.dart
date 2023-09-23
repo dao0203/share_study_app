6e5e6e8e09ea5f54e391bc61b3b75366231a466b
dao0203 <youzaizuoteng872@gmail.com>
2023-09-23 18:11:37 +0900

    set notification screen and indicate paging data

--- lib/ui/view/norification/notification_screen.dart
+++ lib/ui/view/norification/notification_screen.dart
@@ -1,10 +1,68 @@
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:infinite_scroll_pagination/infinite_scroll_pagination.dart';
+import 'package:logger/logger.dart';
+import 'package:share_study_app/data/domain/notification.dart' as notification;
+import 'package:share_study_app/data/repository/di/repository_providers.dart';
 
-class NotificationScreen extends HookConsumerWidget {
+class NotificationScreen extends StatefulHookConsumerWidget {
   const NotificationScreen({super.key});
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    return Container();
+  ConsumerState<NotificationScreen> createState() => _NotificationScreenState();
+}
+
+class _NotificationScreenState extends ConsumerState<NotificationScreen> {
+  static const int _pageSize = 10;
+  final PagingController<int, notification.Notification> _pagingController =
+      PagingController(firstPageKey: 0);
+
+  @override
+  void initState() {
+    super.initState();
+    _pagingController.addPageRequestListener((pageKey) {
+      _fetchPage(pageKey);
+    });
+  }
+
+  Future<void> _fetchPage(int pageKey) async {
+    try {
+      final newItems = await ref
+          .watch(notificationRepositoryProvider)
+          .getWithPagination(pageKey, _pageSize);
+      final isLastPage = newItems.length < _pageSize;
+      if (isLastPage) {
+        _pagingController.appendLastPage(newItems);
+      } else {
+        final nextPageKey = pageKey + newItems.length;
+        _pagingController.appendPage(newItems, nextPageKey);
+      }
+    } catch (error, stacktrace) {
+      Logger().e('getWithPagination.error: $error $stacktrace');
+      _pagingController.error = error;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('通知'),
+      ),
+      body: RefreshIndicator(
+        onRefresh: () => Future.sync(
+          () => _pagingController.refresh(),
+        ),
+        child: PagedListView<int, notification.Notification>(
+          pagingController: _pagingController,
+          builderDelegate: PagedChildBuilderDelegate<notification.Notification>(
+            itemBuilder: (context, notification, index) => Column(
+              children: [
+                Text(notification.message),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
