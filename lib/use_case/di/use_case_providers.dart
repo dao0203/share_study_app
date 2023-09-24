8a032e292e5d50910f6e9147f820c0ef292916b9
dao0203 <youzaizuoteng872@gmail.com>
2023-09-24 18:39:10 +0900

    inject use case

--- lib/use_case/di/use_case_providers.dart
+++ lib/use_case/di/use_case_providers.dart
@@ -1,5 +1,6 @@
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:share_study_app/data/repository/di/repository_providers.dart';
+import 'package:share_study_app/use_case/get_my_notifications_use_case.dart';
 import 'package:share_study_app/use_case/get_my_profile_use_case.dart';
 import 'package:share_study_app/use_case/set_up_splash_use_case.dart';
 
@@ -14,5 +15,12 @@ final setUpSplashUseCaseProvider = Provider<SetUpSplashUseCase>((ref) {
   return SetUpSplashUseCase(
     ref.watch(userAuthRepositoryProvider),
     ref.watch(getMyProfileUseCaseProvider),
+  );
+});
+
+final getMyNotificationsWithPaginationUseCaseProvider =
+    Provider<GetMyNotificationsWithPaginationUseCase>((ref) {
+  return GetMyNotificationsWithPaginationUseCase(
+    ref.watch(notificationRepositoryProvider),
   );
 });
