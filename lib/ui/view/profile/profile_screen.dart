abd4f7b946a7d4b7a10774f8b71d4a903d570dee
dao0203 <youzaizuoteng872@gmail.com>
2023-09-26 01:07:34 +0900

    仮のProfileScreenを追加

---
+++ lib/ui/view/profile/profile_screen.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+class ProfileScreen extends StatefulHookConsumerWidget {
+  const ProfileScreen({super.key});
+
+  @override
+  ConsumerState<ProfileScreen> createState() => _ProfileScreenState();
+}
+
+class _ProfileScreenState extends ConsumerState<ProfileScreen>
+    with SingleTickerProviderStateMixin {
+  late final TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 2, initialIndex: 0, vsync: this);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('プロフィール'),
+        //質問と実績の切り替えるバーを作る
+        bottom: TabBar(
+          controller: _tabController,
+          tabs: [
+            Tab(text: '質問'),
+            Tab(text: '実績'),
+          ],
+        ),
+      ),
+      body: TabBarView(
+        controller: _tabController,
+        children: [
+          //質問
+          ListView.builder(
+            itemCount: 10,
+            itemBuilder: (context, index) {
+              return ListTile(
+                title: Text('質問タイトル'),
+                subtitle: Text('質問内容'),
+                trailing: Text('回答数'),
+              );
+            },
+          ),
+          //実績
+          ListView.builder(
+            itemCount: 10,
+            itemBuilder: (context, index) {
+              return ListTile(
+                title: Text('実績タイトル'),
+                subtitle: Text('実績内容'),
+                trailing: Text('獲得ポイント'),
+              );
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
