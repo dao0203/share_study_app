a682cae37d6798d6d7d9fc3c9331861519aee6f4
dao0203 <youzaizuoteng872@gmail.com>
2023-09-26 13:13:11 +0900

    Cardをやめる

--- lib/ui/components/question_item.dart
+++ lib/ui/components/question_item.dart
@@ -15,92 +15,89 @@ class QuestionItem extends HookConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return GestureDetector(
+      behavior: HitTestBehavior.translucent,
       onTap: () => onPressed(),
-      child: Card(
-        color: Theme.of(context).colorScheme.surface,
-        elevation: 3,
-        child: Row(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Align(
-                alignment: Alignment.bottomLeft,
-                child: question.questioner.imageUrl != null
-                    ? CircleAvatar(
-                        backgroundImage:
-                            NetworkImage(question.questioner.imageUrl!),
-                        //サイズ
-                        radius: 20,
-                      )
-                    : Icon(
-                        Icons.person_outline_outlined,
-                        size: 40,
-                        color: Theme.of(context).colorScheme.onSurface,
-                      ),
-              ),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Align(
+              alignment: Alignment.bottomLeft,
+              child: question.questioner.imageUrl != null
+                  ? CircleAvatar(
+                      backgroundImage:
+                          NetworkImage(question.questioner.imageUrl!),
+                      //サイズ
+                      radius: 20,
+                    )
+                  : Icon(
+                      Icons.person_outline_outlined,
+                      size: 40,
+                      color: Theme.of(context).colorScheme.onSurface,
+                    ),
             ),
-            Expanded(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: [
-                      Text(
-                        question.questioner.nickname,
-                        style: const TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 16,
-                        ),
+          ),
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Text(
+                      question.questioner.nickname,
+                      style: const TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16,
                       ),
-                      const SizedBox(width: 8),
-                      Text(
-                        '${question.createdAt.month}月${question.createdAt.day}日',
-                        style: const TextStyle(
-                          fontSize: 12,
-                        ),
+                    ),
+                    const SizedBox(width: 8),
+                    Text(
+                      '${question.createdAt.month}月${question.createdAt.day}日',
+                      style: const TextStyle(
+                        fontSize: 12,
                       ),
-                      const SizedBox(width: 8),
-                      Expanded(
-                        child: Align(
-                          alignment: Alignment.bottomRight,
-                          child: Text(
-                            question.subjectName,
-                            style: const TextStyle(
-                              fontSize: 12,
-                            ),
+                    ),
+                    const SizedBox(width: 8),
+                    Expanded(
+                      child: Align(
+                        alignment: Alignment.bottomRight,
+                        child: Text(
+                          question.subjectName,
+                          style: const TextStyle(
+                            fontSize: 12,
                           ),
                         ),
                       ),
-                      const SizedBox(width: 16),
-                    ],
-                  ),
-                  Text(
-                    question.title,
-                    style: const TextStyle(
-                      fontSize: 18,
                     ),
+                    const SizedBox(width: 16),
+                  ],
+                ),
+                Text(
+                  question.title,
+                  style: const TextStyle(
+                    fontSize: 18,
                   ),
-                ],
-              ),
+                ),
+              ],
             ),
-            Padding(
-              padding: const EdgeInsets.all(4.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.end,
-                children: [
-                  Icon(
-                    question.isResolved
-                        ? Icons.check_box
-                        : Icons.check_circle_outline,
-                    color: question.isResolved ? Colors.green : Colors.grey,
-                  ),
-                ],
-              ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(4.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                Icon(
+                  question.isResolved
+                      ? Icons.check_box
+                      : Icons.check_circle_outline,
+                  color: question.isResolved ? Colors.green : Colors.grey,
+                ),
+              ],
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
