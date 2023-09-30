02304cabb7fa886e422a90da7ade77dd1f18eb7d
dao0203 <youzaizuoteng872@gmail.com>
2023-09-30 20:10:56 +0900

    followingStateの追加

---
+++ lib/ui/state/is_following_state.dart
@@ -0,0 +1,12 @@
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+import 'package:share_study_app/data/repository/di/repository_providers.dart';
+
+part 'is_following_state.g.dart';
+
+@riverpod
+class IsFollowingState extends _$IsFollowingState {
+  @override
+  Stream<bool> build(String profileId) {
+    return ref.watch(profileRepositoryProvider).isFollowing(profileId);
+  }
+}
