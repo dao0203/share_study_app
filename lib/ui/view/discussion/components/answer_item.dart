c94202faf7e1fd37230f85a06b7c055bab1b8f39
Mgtantheta <freecityNJ@gmail.com>
2023-10-02 23:32:48 +0900

    WIP 全体長押し

--- lib/ui/view/discussion/components/answer_item.dart
+++ lib/ui/view/discussion/components/answer_item.dart
@@ -10,146 +10,149 @@ class AnswerItem extends HookConsumerWidget {
   final Answer answer;
   final supabase = Supabase.instance.client;
   final Function() onIconPressed;
+
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return GestureDetector(
+      behavior: HitTestBehavior.translucent,
         onLongPress: () {
-      showDialog(
-        context: context,
-        builder: (_) {
-          return AlertDialog(
-            title: const Text('ベストアンサーにしますか？'),
-            actions: [
-              TextButton(
-                onPressed: () {
-                  // 画面遷移せずにアラートを閉じる
-                  Navigator.pop(context);
-                },
-                child: const Text('キャンセル'),
-              ),
-              TextButton(
-                onPressed: () async {
-                  // ref.read(updateAnswerUseCaseProvider);
-                  //is_best_answerをtrueにして、answersテーブルを更新する
-                  await supabase
-                      .from('answers')
-                      .update({'is_best_answer': true})
-                      .eq('id', answer.id)
-                      .then((value) => Navigator.pop(context));
-                },
-                child: const Text('OK'),
-              ),
-            ],
-          );
-        },
-      );
-    },
-    child:
-      Row(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: GestureDetector(
-            behavior: HitTestBehavior.opaque,
-            onTap: () => onIconPressed(),
-            // ベストアンサーにするかどうかのアラートを表示する
-            onLongPress: () {
-              showDialog(
-                context: context,
-                builder: (_) {
-                  return AlertDialog(
-                    title: const Text('ベストアンサーにしますか？'),
-                    actions: [
-                      TextButton(
-                        onPressed: () {
-                          // 画面遷移せずにアラートを閉じる
-                          Navigator.pop(context);
-                        },
-                        child: const Text('キャンセル'),
-                      ),
-                      TextButton(
-                        onPressed: () async {
-                          // ref.read(updateAnswerUseCaseProvider);
-                          //is_best_answerをtrueにして、answersテーブルを更新する
-                          await supabase
-                              .from('answers')
-                              .update({'is_best_answer': true}).eq('id', answer.id).then((value) => Navigator.pop(context));
-                        },
-                        child: const Text('OK'),
-                      ),
-                    ],
-                  );
-                },
+          showDialog(
+            context: context,
+            builder: (_) {
+              return AlertDialog(
+                title: const Text('ベストアンサーにしますか？'),
+                actions: [
+                  TextButton(
+                    onPressed: () {
+                      // 画面遷移せずにアラートを閉じる
+                      Navigator.pop(context);
+                    },
+                    child: const Text('キャンセル'),
+                  ),
+                  TextButton(
+                    onPressed: () async {
+                      // ref.read(updateAnswerUseCaseProvider);
+                      //is_best_answerをtrueにして、answersテーブルを更新する
+                      await supabase
+                          .from('answers')
+                          .update({'is_best_answer': true})
+                          .eq('id', answer.id)
+                          .then((value) => Navigator.pop(context));
+                    },
+                    child: const Text('OK'),
+                  ),
+                ],
               );
             },
-          child: Align(
-              alignment: Alignment.bottomLeft,
-              child: answer.answerer.imageUrl == null
-                  ? CircleAvatar(
-                      backgroundColor: Theme.of(context).colorScheme.background,
-                      backgroundImage: const NetworkImage(
-                          //ミャウミャウ
-                          'https://purr.objects-us-east-1.dream.io/i/1222.jpg'),
-                      radius: 20,
-                    )
-                  : Icon(
-                      Icons.person_outline_outlined,
-                      size: 40,
-                      color: Theme.of(context).colorScheme.onSurface,
-                    ),
+          );
+        },
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: GestureDetector(
+                behavior: HitTestBehavior.opaque,
+                onTap: () => onIconPressed(),
+                // ベストアンサーにするかどうかのアラートを表示する
+                onLongPress: () {
+                  showDialog(
+                    context: context,
+                    builder: (_) {
+                      return AlertDialog(
+                        title: const Text('ベストアンサーにしますか？'),
+                        actions: [
+                          TextButton(
+                            onPressed: () {
+                              // 画面遷移せずにアラートを閉じる
+                              Navigator.pop(context);
+                            },
+                            child: const Text('キャンセル'),
+                          ),
+                          TextButton(
+                            onPressed: () async {
+                              // ref.read(updateAnswerUseCaseProvider);
+                              //is_best_answerをtrueにして、answersテーブルを更新する
+                              await supabase
+                                  .from('answers')
+                                  .update({'is_best_answer': true})
+                                  .eq('id', answer.id)
+                                  .then((value) => Navigator.pop(context));
+                            },
+                            child: const Text('OK'),
+                          ),
+                        ],
+                      );
+                    },
+                  );
+                },
+                child: Align(
+                  alignment: Alignment.bottomLeft,
+                  child: answer.answerer.imageUrl == null
+                      ? CircleAvatar(
+                          backgroundColor:
+                              Theme.of(context).colorScheme.background,
+                          backgroundImage: const NetworkImage(
+                              //ミャウミャウ
+                              'https://purr.objects-us-east-1.dream.io/i/1222.jpg'),
+                          radius: 20,
+                        )
+                      : Icon(
+                          Icons.person_outline_outlined,
+                          size: 40,
+                          color: Theme.of(context).colorScheme.onSurface,
+                        ),
+                ),
+              ),
             ),
-          ),
-        ),
-        Expanded(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Row(
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Text(
-                    answer.answerer.nickname,
-                    style: const TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 16,
-                    ),
+                  Row(
+                    children: [
+                      Text(
+                        answer.answerer.nickname,
+                        style: const TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16,
+                        ),
+                      ),
+                      const SizedBox(width: 8),
+                      Text(
+                        '${answer.createdAt.month}月${answer.createdAt.day}日',
+                        style: const TextStyle(
+                          fontSize: 12,
+                        ),
+                      ),
+                    ],
                   ),
-                  const SizedBox(width: 8),
                   Text(
-                    '${answer.createdAt.month}月${answer.createdAt.day}日',
+                    answer.content,
                     style: const TextStyle(
-                      fontSize: 12,
+                      fontSize: 18,
                     ),
                   ),
                 ],
               ),
-              Text(
-                answer.content,
-                style: const TextStyle(
-                  fontSize: 18,
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                SvgPicture.asset(
+                  answer.isBestAnswer
+                      ? 'assets/icons/crown.svg'
+                      : 'assets/icons/crown-outlined.svg',
+                  colorFilter: ColorFilter.mode(
+                    answer.isBestAnswer
+                        ? Colors.yellow
+                        : Theme.of(context).colorScheme.onSurface,
+                    BlendMode.srcIn,
+                  ),
                 ),
-              ),
-            ],
-          ),
-        ),
-        Column(
-          crossAxisAlignment: CrossAxisAlignment.end,
-          children: [
-            SvgPicture.asset(
-              answer.isBestAnswer
-                  ? 'assets/icons/crown.svg'
-                  : 'assets/icons/crown-outlined.svg',
-              colorFilter: ColorFilter.mode(
-                answer.isBestAnswer
-                    ? Colors.yellow
-                    : Theme.of(context).colorScheme.onSurface,
-                BlendMode.srcIn,
-              ),
+              ],
             ),
           ],
-        ),
-      ],
-    )
-    );
+        ));
   }
 }
