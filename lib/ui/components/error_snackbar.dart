27f397a0d289e6d0695e5f88d7b33263fe6e5edd
Mgtantheta <freecityNJ@gmail.com>
2023-10-07 17:40:42 +0900

    add error Snack bar

--- lib/ui/components/error_snackbar.dart
+++ lib/ui/components/error_snackbar.dart
@@ -5,9 +5,34 @@ class ErrorSnackBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const SnackBar(
-      backgroundColor: Colors.red,
-      content: Text('エラーが発生しました'),
+    return SnackBar(
+      padding: const EdgeInsetsDirectional.symmetric(horizontal: 16),
+      margin: const EdgeInsetsDirectional.all(16),
+      content: const Padding(
+        padding: EdgeInsets.all(8.0),
+        child: Row(
+          children: [
+            Icon(
+              Icons.cancel,
+              color: Colors.red,
+            ),
+            Text(
+              '質問の投稿に失敗しました: error',
+              style: TextStyle(color: Colors.red),
+            ),
+          ],
+        ),
+      ),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10),
+      ),
+      behavior: SnackBarBehavior.floating,
+      showCloseIcon: true,
+      elevation: 4.0,
+      backgroundColor: Colors.white,
+      closeIconColor: Colors.red,
+      clipBehavior: Clip.hardEdge,
+      dismissDirection: DismissDirection.horizontal,
     );
   }
 }
