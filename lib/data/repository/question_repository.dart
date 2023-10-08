1f99b6e99378437941bf864528d31af7577d0ac9
dao0203 <youzaizuoteng872@gmail.com>
2023-10-08 14:49:51 +0900

    add image path to question repository interface

--- lib/data/repository/question_repository.dart
+++ lib/data/repository/question_repository.dart
@@ -3,7 +3,8 @@ import 'package:share_study_app/data/domain/question.dart';
 abstract interface class QuestionRepository {
   Future<List<Question>> getAll();
   Future<Question> get(String id);
-  Future<void> add(String title, String content, String subjectName);
+  Future<void> add(
+      String title, String content, String subjectName, String? path);
   Future<List<Question>> getWithPagination(int start, int end);
   Future<List<Question>> getWithPaginationAndKeyword(
       int start, int end, String keyword);
