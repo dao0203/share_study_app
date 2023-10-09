6b2cbbc742f7b78a0c496cc71ec6a5e14872ea38
dao0203 <youzaizuoteng872@gmail.com>
2023-10-09 22:13:33 +0900

    delete user function

--- lib/data/repository/supabase_user_auth_repository.dart
+++ lib/data/repository/supabase_user_auth_repository.dart
@@ -52,13 +52,29 @@ final class SupabaseUserAuthRepository implements UserAuthRepository {
 
   @override
   Future<void> delete() async {
-    await Supabase.instance.client.auth.admin
-        .deleteUser(getCurrentUser()!.id)
-        .then((value) {
+    await Supabase.instance.client.rpc('delete_user').then((value) async {
       Logger().d('delete user');
     }).onError((error, stackTrace) {
       Logger().e(error.toString() + stackTrace.toString());
       throw Exception('delete user failed');
+    }).whenComplete(() async {
+      await _client.signOut();
     });
   }
 }
+
+// delete関数がなぜこうなっているか
+// 参考：https://supabase.com/docs/guides/auth/managing-user-data#deleting-users
+//
+// supabase.instance.auth.admin.deleteUser(user.id);はしようできない、かつ
+// クライアント側からは、ユーザーを削除することができない
+// rpcを使用してsupabase側で作成した関数,delete_userを呼び出している
+//
+// この関数は、supabaseのauth.usersテーブルからauth.uidを使用してユーザーを削除する
+// auth.uidはprofiles.idと一致するため、profilesテーブルからprofiles.idを使用してユーザーを削除することもできる
+// すなわち、profilesテーブルのデータが削除されると、全ての関連するデータが削除される
+// 
+// delete関数の中でsignOut関数を呼び出しているか
+// これは、delete関数を呼び出すと、ユーザーが削除されるが、
+// クライアント側に削除されたはずのユーザーの情報が残ってしまうため、
+// クライアント側のユーザー情報を削除するためにsignOut関数を呼び出している
